44ed9a6cf35298633dde2d855443b79c4c60da0d
Ankur Charan <ankurcharan98@gmail.com>
2018-10-20 20:15:46 +0530

    login request
    Signed-off-by: Ankur Charan <ankurcharan98@gmail.com>

--- myjs/googleAuth.js
+++ myjs/googleAuth.js
@@ -1,17 +1,42 @@
 function onSignIn(googleUser) {
-  var profile = googleUser.getBasicProfile();
-  console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-  console.log('Name: ' + profile.getName());
-  console.log('Image URL: ' + profile.getImageUrl());
-  console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
 
-  console.log(googleUser.getAuthResponse().id_token);
+	var profile = googleUser.getBasicProfile();
+	// console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+	// console.log('Name: ' + profile.getName());
+	// console.log('Image URL: ' + profile.getImageUrl());
+	console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+
+	console.log(googleUser.getAuthResponse().id_token);
+
+	let token = googleUser.getAuthResponse().id_token;
+
+	$.ajax({
+
+		url: "https://us-central1-primehackathon.cloudfunctions.net/api/login",
+		type: "POST",
+		data: {
+			"idToken": token
+		},
+		success: function(result, status) {
+
+			console.log("query baji");
+			if(status === 'success') {
+
+				console.log(result);
+			}
+			else {
+				signOut();
+				console.log("error occured while requesting");
+			}
+
+		}
+	})
 }
 
 /* <a href="#" onclick="signOut();">Sign out</a> */
 function signOut() {
-    var auth2 = gapi.auth2.getAuthInstance();
-    auth2.signOut().then(function () {
-      console.log('User signed out.');
-    });
-  }
+	var auth2 = gapi.auth2.getAuthInstance();
+	auth2.signOut().then(function () {
+		console.log('User signed out.');
+	});
+}
